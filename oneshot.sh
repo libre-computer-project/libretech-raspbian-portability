384460f62e739eadbf562868b1e3f494b8ae3e70
Da Xue <da@lessconfused.com>
2022-07-06 03:06:58 -0400

    oneshot buster lite support

--- oneshot.sh
+++ oneshot.sh
@@ -62,3 +62,56 @@ elif [ "${TARGET_OS_RELEASE[VERSION_ID]}" != '"10"' ]; then
 	echo "os-release: only Raspbian 10 is supported!" >&2
 	exit 1
 fi
+
+DPKG_ARCH_TARGET=arm64
+dpkg_arch=$(dpkg --print-architecture)
+if [ -z "$dpkg_arch" ]; then
+	echo "dpkg: failed to get architecture" >&2
+	exit 1
+elif [ "$dpkg_arch" != "armhf" ]; then
+	echo "dpkg: architecture is not supported" >&2
+	exit 1
+fi
+
+dpkg_arches_foreign=$(dpkg --print-foreign-architectures)
+for dpkg_arch_foreign in $dpkg_arches_foreign; do
+	if [ "$dpkg_arch_foreign" = "$DPKG_ARCH_TARGET" ]; then
+		break
+	fi
+done
+if [ "$dpkg_arch_foreign" != "$DPKG_ARCH_TARGET" ]; then
+	dpkg --add-architecture arm64
+fi
+
+KERNEL_HEADER_URL='https://kernel.libre.computer/manual/linux-5.18/linux-header-5.18_arm64.deb'
+KERNEL_IMAGE_URL='https://kernel.libre.computer/manual/linux-5.18/linux-image-5.18_arm64.deb'
+kernel_dir=$(mktemp -d)
+wget -O "$kernel_dir/${KERNEL_HEADER_URL##*/}" "$KERNEL_HEADER_URL"
+wget -O "$kernel_dir/${KERNEL_IMAGE_URL##*/}" "$KERNEL_IMAGE_URL"
+
+dpkg -i "$kernel_dir"/*.deb
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+
+apt_sources=$(ls /etc/apt/sources.list /etc/apt/sources.list.d/*.list)
+for apt_source in $apt_sources; do
+	sed -Ei "s/^(deb)\\s+(http:\\/\\/)/\\1 [ arch=armhf ] \2/" "$apt_source"
+done
+echo 'deb [ arch=arm64 ] http://deb.debian.org/debian/ buster main' > /etc/apt/sources.list.d/debian-main.list
+
+apt update
+#apt -y dist-upgrade
+apt -y install grub-efi-arm64
+grub-install --directory=/usr/lib/grub/arm64-efi --efi-directory=/boot --force-extra-removable --no-nvram
+sed -Ei "s/(GRUB_CMDLINE_LINUX_DEFAULT)=\"quiet/\1=\"noquiet/" /etc/default/grub
+update-grub
+mkdir -p /boot/EFI/debian
+cp /boot/EFI/raspbian/grub.cfg /boot/EFI/debian/grub.cfg
+
+BOOT_LOADER_URL='https://boot.libre.computer/manual/u-boot-latest-aml-s905x-cc'
+boot_loader_file=$(mktemp)
+wget -O "$boot_loader_file" "$BOOT_LOADER_URL"
+dd if="$boot_loader_file" of=/dev/"$TARGET_DISK" bs=512 seek=1
+
