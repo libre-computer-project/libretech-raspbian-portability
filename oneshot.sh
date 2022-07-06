63b617ff6a3943d2de6050b7251905e9b0f02ed8
Da Xue <da@lessconfused.com>
2022-07-06 19:05:48 -0400

    oneshot ubuntu jammy support

--- oneshot.sh
+++ oneshot.sh
@@ -20,7 +20,8 @@ This script installs/configures/overwrites data this device/MicroSD card to
 support Libre Computer AML-S905X-CC and AML-S805X-AC. It is designed to run on 
 a Raspberry Pi(R) and requires internet access to download components.
 
-Only Raspbian 10 Buster 32-bit armhf lite and desktop are currently supported.
+Only Raspbian 10 Buster 32-bit armhf lite and desktop and Ubuntu 22.04 Jammy
+preinstalled desktop image are currently supported.
 Once completed, move the MicroSD card to one of the Libre Computer boards.
 
 Please type 'continue' without quotes to acknowledge and start the script.
@@ -79,32 +80,64 @@ for line in "${lines[@]}"; do
 	TARGET_OS_RELEASE["$key"]="$value"
 done
 
-if [ "${TARGET_OS_RELEASE[ID]}" != "raspbian" ]; then
-	echo "os-release: installed distro is not Raspbian!" >&2
-	exit 1
-elif [ "${TARGET_OS_RELEASE[VERSION_ID]}" != '"10"' ]; then
+if [ "${TARGET_OS_RELEASE[ID]}" = "raspbian" -a "${TARGET_OS_RELEASE[VERSION_ID]}" = '"10"' ]; then
+	DPKG_ARCH_TARGET=arm64
+	grub_install_cmd="grub-install --directory=/usr/lib/grub/arm64-efi --efi-directory=/boot --force-extra-removable --no-nvram"
+elif [ "${TARGET_OS_RELEASE[ID]}" = "ubuntu" -a "${TARGET_OS_RELEASE[VERSION_ID]}" = '"22.04"' ]; then
+	grub_install_cmd="grub-install --directory=/usr/lib/grub/arm64-efi --efi-directory=/boot/firmware --no-nvram"
+	#chmod -x /etc/kernel/postinst.d/zz-flash-kernel
+else
 	echo "os-release: only Raspbian 10 is supported!" >&2
 	exit 1
 fi
 
-DPKG_ARCH_TARGET=arm64
 dpkg_arch=$(dpkg --print-architecture)
 if [ -z "$dpkg_arch" ]; then
 	echo "dpkg: failed to get architecture" >&2
 	exit 1
-elif [ "$dpkg_arch" != "armhf" ]; then
+elif [ "$dpkg_arch" = "armhf" ]; then
+	dpkg_arches_foreign=$(dpkg --print-foreign-architectures)
+	for dpkg_arch_foreign in $dpkg_arches_foreign; do
+		if [ "$dpkg_arch_foreign" = "$DPKG_ARCH_TARGET" ]; then
+			break
+		fi
+	done
+	if [ "$dpkg_arch_foreign" != "$DPKG_ARCH_TARGET" ]; then
+		dpkg --add-architecture arm64
+	fi
+	
+	apt_sources=$(ls /etc/apt/sources.list /etc/apt/sources.list.d/*.list)
+	for apt_source in $apt_sources; do
+		sed -Ei "s/^(deb)\\s+(http:\\/\\/)/\\1 [ arch=armhf ] \2/" "$apt_source"
+	done
+	echo 'deb [ arch=arm64 ] http://deb.debian.org/debian/ buster main' > /etc/apt/sources.list.d/debian-main.list
+	
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+
+elif [ "$dpkg_arch" = "arm64" ]; then
+	:
+else
 	echo "dpkg: architecture is not supported" >&2
 	exit 1
 fi
 
-dpkg_arches_foreign=$(dpkg --print-foreign-architectures)
-for dpkg_arch_foreign in $dpkg_arches_foreign; do
-	if [ "$dpkg_arch_foreign" = "$DPKG_ARCH_TARGET" ]; then
-		break
+if [ "${TARGET_OS_RELEASE[ID]}" = "ubuntu" -a "${TARGET_OS_RELEASE[VERSION_ID]}" = '"22.04"' ]; then
+	apt -y remove flash-kernel
+	if [ -f /boot/firmware/boot.scr ]; then
+		rm /boot/firmware/boot.scr
 	fi
-done
-if [ "$dpkg_arch_foreign" != "$DPKG_ARCH_TARGET" ]; then
-	dpkg --add-architecture arm64
+	dtbs=$(ls /boot/dtb /boot/dtb-* /boot/dtbs)
+	if [ ! -z "$dtbs" ]; then
+		for dtb in $dtbs; do
+			if [ -f "$dtb" -o -L "$dtb" ]; then
+				rm "$dtb"
+			elif [ -d "$dtb" ]; then
+				rm -r "$dtb"
+			fi
+		done
+	fi
 fi
 
 KERNEL_HEADER_URL='https://kernel.libre.computer/manual/linux-5.18/linux-header-5.18_arm64.deb'
@@ -115,24 +148,16 @@ wget -O "$kernel_dir/${KERNEL_IMAGE_URL##*/}" "$KERNEL_IMAGE_URL"
 
 dpkg -i "$kernel_dir"/*.deb
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-
-apt_sources=$(ls /etc/apt/sources.list /etc/apt/sources.list.d/*.list)
-for apt_source in $apt_sources; do
-	sed -Ei "s/^(deb)\\s+(http:\\/\\/)/\\1 [ arch=armhf ] \2/" "$apt_source"
-done
-echo 'deb [ arch=arm64 ] http://deb.debian.org/debian/ buster main' > /etc/apt/sources.list.d/debian-main.list
-
 apt update
 #apt -y dist-upgrade
 apt -y install grub-efi-arm64
-grub-install --directory=/usr/lib/grub/arm64-efi --efi-directory=/boot --force-extra-removable --no-nvram
+$grub_install_cmd
 sed -Ei "s/(GRUB_CMDLINE_LINUX_DEFAULT)=\"quiet/\1=\"noquiet/" /etc/default/grub
 update-grub
-mkdir -p /boot/EFI/debian
-cp /boot/EFI/raspbian/grub.cfg /boot/EFI/debian/grub.cfg
+if [ "${TARGET_OS_RELEASE[ID]}" = "raspbian" ]; then
+	mkdir -p /boot/EFI/debian
+	cp /boot/EFI/raspbian/grub.cfg /boot/EFI/debian/grub.cfg
+fi
 
 BOOT_LOADER_URL='https://boot.libre.computer/manual/u-boot-latest-aml-s905x-cc'
 boot_loader_file=$(mktemp)
@@ -148,5 +173,5 @@ Section "Device"
 EndSection
 EOF
 
-read -n 1 -p "Modifications complete. Press any key to shutdown. Once the green LED stops blinking, move the MicroSD card to the Libre Computer board."
+read -n 1 -p "Modifications complete. Press any key to shutdown. Once the green LED stops blinking and turns off for 10 seconds, remove power and move the MicroSD card to the Libre Computer board."
 shutdown -H now
--- readme.md
+++ readme.md
@@ -1,20 +1,23 @@
 # Libre Computer Raspbian Portability
 ## Objective
-This repository contains scripts that allows Libre Computer boards to run existing Raspbian images.
-It uses an upstream bootloader and kernel configured to support armhf executables. The resulting
-image should boot both on the original board and the Libre Computer board.
+This repository contains scripts that allows Libre Computer boards to run existing images targeted at the Raspberry Pi:registered: board family.
+It uses an upstream bootloader and kernel configured to support armhf executables. 
+
+## Supported Distributions
+- [Raspbian 10 Buster Lite armhf](https://www.raspberrypi.com/software/operating-systems/#raspberry-pi-os-32-bit) - Dual Board Boot Capable
+- [Raspbian 10 Buster Desktop armhf](https://www.raspberrypi.com/software/operating-systems/#raspberry-pi-os-32-bit) - Dual Board Boot Capable
+- [Ubuntu 22.04 Jammy Preinstalled Desktop arm64](https://cdimage.ubuntu.com/releases/22.04/release/)
+
+## Supported Boards
+- AML-S905X-CC Le Potato
+- AML-S805X-AC La Frite
+- ROC-RK3399-PC (with firmware)
+- Other Libre Computer boards will be supported soon
 
 ## Current Status
-This is at the proof of concept stage and is tested against Raspbian Lite and Desktop based on
-the Debian 10 Buster release. The code base needs to be refined and modularized over time to
-support all of the Raspbian variations that exist. We highly recommend that you back up your data!
-
-It supports the following Libre Computer boards:
-1. AML-S905X-CC Le Potato
-2. AML-S805X-AC La Frite
-3. ROC-RK3399-PC (with firmware)
-
-Support can easily be added for all Libre Computer boards.
+This is at the proof of concept stage and is tested against the distribution
+The code base needs to be refined and modularized over time to support all of the Raspbian variations that exist. 
+We highly recommend that you back up your data!
 
 ## Limitations
 - This does not use Raspbian's kernels.
@@ -24,13 +27,13 @@ Support can easily be added for all Libre Computer boards.
 - As a POC, the code is very ad-hoc and not representative of our normal coding standards.
 
 ## How to Use
-On your Raspberry Pi, run:
+On your Raspberry Pi:registered:, run:
 ```bash
 git clone https://github.com/libre-computer-project/libretech-raspbian-portability.git
 cd libretech-raspbian-portability
 sudo ./oneshot.sh
 ```
-Follow the instructions. Move MicroSD card to one of the Libre Computer board.
+Follow the instructions.
 
 ## Help
 [Libera Chat IRC #librecomputer](https://web.libera.chat/#librecomputer)
@@ -39,6 +42,8 @@ Follow the instructions. Move MicroSD card to one of the Libre Computer board.
 Our core work is on upstream Linux and u-boot so scripts such as this are not a high priority.
 - Raspbian 11 bullseye 64-bit and 32-bit support
 - ALL-H3-CC H2+/H3/H5 support
+- ROC-RK3328-CC support
 - Refactor to robust coding standards
+- Device tree overlay translation support as another project
 
-If you need commercial support for 32-bit Raspbian or any distro, [please let us know](https://libre.computer/#contact).
+If you need commercial support for any distro, [please let us know](https://libre.computer/#contact).
